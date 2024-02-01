e0faad78f1f22f90392a4fcee50e76cd3b906f98
Luka Holota <holotaluka@gmail.con>
2024-02-01 09:39:51 +0200

    multiple levels classes and spells html

--- app/logic/SpellsFilters.py
+++ app/logic/SpellsFilters.py
@@ -2,10 +2,10 @@ class SpellsFilters:
     def __init__(self, form):
         self.form = form
         self.name_filter = form.get('name', '')
-        self.class_filter = form.get('class', '')
-        self.level_filter = form.get('level', '')
+        self.classes_filter = form.getlist('classes')
+        self.levels_filter = form.getlist('levels')
         self.school_filter = form.get('school', '')
         self.casting_time_filter = form.get('casting_time', '')
         self.ritual_filter = form.get('ritual', '')
         self.concentration_filter = form.get('concentration', '')
-        self.source_filter = form.get('source', '')
+        self.sources_filter = form.getlist('sources')
--- app/logic/SpellsListLoader.py
+++ app/logic/SpellsListLoader.py
@@ -1,6 +1,7 @@
 from app.models import Spell
 from app.models import SpellClasses
 from app.logic.SpellsFilters import SpellsFilters
+from sqlalchemy import case
 
 
 class SpellsListLoader:
@@ -11,7 +12,9 @@ class SpellsListLoader:
     def load(self):
         query = Spell.query
         query = self.add_filters(query)
-        query = query.order_by(Spell.name)
+
+        is_cantrip = case((Spell.level == 'замовляння', 0), else_=1)
+        query = query.order_by(is_cantrip, Spell.level, Spell.name)
         self.spells = query.all()
         self.reduce_casting_time()
 
@@ -19,11 +22,11 @@ class SpellsListLoader:
         if self.filters.name_filter:
             query = query.filter(Spell.name.ilike('%' + self.filters.name_filter + '%'))
 
-        if self.filters.class_filter:
-            query = query.filter(Spell.classes.any(SpellClasses.class_name == self.filters.class_filter))
+        if self.filters.classes_filter:
+            query = query.filter(Spell.classes.any(SpellClasses.class_name.in_(self.filters.classes_filter)))
 
-        if self.filters.level_filter:
-            query = query.filter(Spell.level.ilike('%' + self.filters.level_filter + '%'))
+        if self.filters.levels_filter:
+            query = query.filter(Spell.level.in_(self.filters.levels_filter))
 
         if self.filters.school_filter:
             query = query.filter(Spell.school.ilike('%' + self.filters.school_filter + '%'))
@@ -37,8 +40,8 @@ class SpellsListLoader:
         if self.filters.concentration_filter:
             query = query.filter(Spell.has_concentration.ilike('%' + self.filters.concentration_filter + '%'))
 
-        if self.filters.source_filter:
-            query = query.filter(Spell.source.ilike('%' + self.filters.source_filter + '%'))
+        if self.filters.sources_filter:
+            query = query.filter(Spell.source.in_(self.filters.sources_filter))
 
         return query
 
